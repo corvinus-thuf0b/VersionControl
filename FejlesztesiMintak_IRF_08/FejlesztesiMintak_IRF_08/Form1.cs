9f82d1bac5c0aa16b9005dc3809967dd385e280e
Rómeó Pribyll <romeo.pribyll@stud.uni-corvinus.hu>
2021-11-05 17:16:55 +0100

    Futószalag

--- FejlesztesiMintak_IRF_08/FejlesztesiMintak_IRF_08/Form1.cs
+++ FejlesztesiMintak_IRF_08/FejlesztesiMintak_IRF_08/Form1.cs
@@ -7,14 +7,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using FejlesztesiMintak_IRF_08.Entities;
 
 namespace FejlesztesiMintak_IRF_08
 {
     public partial class Form1 : Form
     {
+        List<Ball> _balls = new List<Ball>();
+
+        private BallFactory _factory;
+        BallFactory Factory
+        {
+            get { return _factory; }
+            set { _factory = value; }
+        }
+
         public Form1()
         {
             InitializeComponent();
+            Factory = new BallFactory();
+        }
+
+        private void createTimer_Tick(object sender, EventArgs e)
+        {
+            var ball = Factory.CreateNew();
+            _balls.Add(ball);
+            ball.Left = -ball.Width;
+            mainPanel.Controls.Add(ball);
+        }
+
+        private void conveyorTimer_Tick(object sender, EventArgs e)
+        {
+            var maxPosition = 0;
+            foreach (var item in _balls)
+            {
+                item.MoveBall();
+                if (item.Left > maxPosition)
+                {
+                    maxPosition = item.Left;
+                }
+            }
+
+            if (maxPosition >= 1000)
+            {
+                var oldestBall = _balls[0];
+                mainPanel.Controls.Remove(oldestBall);
+                _balls.Remove(oldestBall);
+            }
         }
     }
 }
