8c6e530f51b9adb371395e42b0c56b6b124b4c00
Rómeó Pribyll <romeo.pribyll@stud.uni-corvinus.hu>
2021-11-05 16:48:59 +0100

    Labda létrehozása

---
+++ FejlesztesiMintak_IRF_08/FejlesztesiMintak_IRF_08/Entities/Ball.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FejlesztesiMintak_IRF_08.Entities
+{
+    class Ball : Label
+    {
+        public Ball()
+        {
+            Height = 50;
+            Width = 50;
+            AutoSize = false;
+
+            Paint += Ball_Paint;
+        }
+
+        private void Ball_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+
+        protected void DrawImage(Graphics g)
+        {
+            g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
+        }
+
+        public void MoveBall()
+        {
+            Left += 1;
+        }
+    }
+}
