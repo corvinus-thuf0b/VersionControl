67409249577d551c884fa04cb434daba8f7ff5b8
Rómeó Pribyll <romeo.pribyll@stud.uni-corvinus.hu>
2021-11-09 19:08:37 +0100

    IToyFactory

---
+++ FejlesztesiMintak_IRF_08/FejlesztesiMintak_IRF_08/Abstactions/IToyFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FejlesztesiMintak_IRF_08.Abstactions
+{
+    interface IToyFactory
+    {
+        Toy CreateNew();
+    }
+}
