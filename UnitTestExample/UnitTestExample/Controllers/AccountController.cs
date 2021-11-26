e34ca4b1ecb5b79453c028468ab457e5171d31b9
Rómeó Pribyll <romeo.pribyll@stud.uni-corvinus.hu>
2021-11-26 09:02:52 +0100

    Jelszó validálás függvény

--- UnitTestExample/UnitTestExample/Controllers/AccountController.cs
+++ UnitTestExample/UnitTestExample/Controllers/AccountController.cs
@@ -25,7 +25,7 @@ namespace UnitTestExample.Controllers
             if(!ValidateEmail(email))
                 throw new ValidationException(
                     "A megadott e-mail cím nem megfelelő!");
-            if(!ValidateEmail(email))
+            if(!ValidatePassword(password))
                 throw new ValidationException(
                     "A megadottt jelszó nem megfelelő!\n" +
                     "A jelszó legalább 8 karakter hosszú kell legyen, csak az angol ABC betűiből és számokból állhat, és tartalmaznia kell legalább egy kisbetűt, egy nagybetűt és egy számot.");
@@ -50,7 +50,11 @@ namespace UnitTestExample.Controllers
 
         public bool ValidatePassword(string password)
         {
-            return true;
+            var lowerCase = new Regex(@"[a-z]+");
+            var upperCase = new Regex(@"[A-Z]+");
+            var number = new Regex(@"[0-9]+");
+            var length = new Regex(@".{8,}");
+            return lowerCase.IsMatch(password) && upperCase.IsMatch(password) && number.IsMatch(password) && length.IsMatch(password);
         }
     }
 }
